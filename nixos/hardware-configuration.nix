5a68979062631dcfe9fffbf991356d0af4811d96
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-10-14 04:43:07 +0300

    `nixos-generate-config`

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -5,25 +5,25 @@
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
   ];
-
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-
+  
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/fac7e150-ce11-4c07-97ba-1cabf92f4f9a";
+    device = "/dev/disk/by-uuid/d9953242-6f75-4183-b64c-a69919466d07";
     fsType = "ext4";
   };
-
+  
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/7028-14F0";
+    device = "/dev/disk/by-uuid/B3AB-4AD0";
     fsType = "vfat";
     options = [ "fmask=0077" "dmask=0077" ];
   };
-
+  
   swapDevices = [
-    { device = "/dev/disk/by-uuid/c1103f18-abec-403a-8ae3-10c7cd8ba87d"; }
+    { device = "/dev/disk/by-uuid/f666f726-5ef6-426f-bb71-2f0c0dc389e9"; }
   ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
