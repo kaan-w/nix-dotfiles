1a20cdb269fc24db1da8bf6300a84b1590b54daf
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-10-27 21:18:13 +0300

    flake: update

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 		zen-browser = {
-			url = "github:ch4og/zen-browser-flake";
+			url = "github:0xc000022070/zen-browser-flake";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
 	}; 
