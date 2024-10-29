c7ec249c0666d9b760c67f3dfaec3f4774a24133
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-10-29 23:45:52 +0300

    refactor: wallpaper path

--- home/hypr/hyprpaper.nix
+++ home/hypr/hyprpaper.nix
@@ -1,11 +1,13 @@
-{
+let
+	wallpaper = builtins.toString ../../wallpapers/ganyu_window_sill.jpg;
+in {
   services.hyprpaper = {
 		enable = true;
 		settings = {
 			splash = false;
 			ipc = false;
-			preload = [ "~/nix-dotfiles/wallpapers/hakurei_reimu_touhou.jpg" ];
-			wallpaper = [ ", ~/nix-dotfiles/wallpapers/hakurei_reimu_touhou.jpg" ];
+			preload = [ wallpaper ];
+			wallpaper = [ (", " + wallpaper) ];
 		};
 	};
 }
