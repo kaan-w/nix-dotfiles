649efa207d3d2d0bf941590fd74ea7b0c9340b96
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-12-16 22:39:54 +0300

    ags: uninstall

--- flake.nix
+++ flake.nix
@@ -9,10 +9,6 @@
 			url = "github:hyprwm/Hyprland";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		ags = {
-			url = "github:aylur/ags/v2";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
 		zen-browser = {
 			url = "github:0xc000022070/zen-browser-flake";
 			inputs.nixpkgs.follows = "nixpkgs";
--- home/programs/ags.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, ... }: {
-  imports = [
-    inputs.ags.homeManagerModules.default
-  ];
-  programs.ags = {
-    enable = true;
-    configDir = ../../ags;
-  };
-}
