897191ecf2bf43f0c619ead1bce834d96b15a516
zaer1n <105508453+zaer1n@users.noreply.github.com>
2024-12-18 19:14:59 +0300

    fix: gtk focus color change

--- home/stylix.nix
+++ home/stylix.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{ pkgs, config, ... }: {
   stylix = {
     enable = true;
     image = pkgs.fetchurl (builtins.elemAt (import ../wallpapers.nix) 3);
@@ -9,6 +9,10 @@
 	  	size = 24;
     };
     targets = {
+      gtk.extraCss = ''
+        @define-color sidebar_bg_color #${config.stylix.base16Scheme.base00};
+        @define-color headerbar_bg_color #${config.stylix.base16Scheme.base00};
+      '';
       vscode.enable = false;
       vesktop.enable = false;
       spicetify.enable = false;
