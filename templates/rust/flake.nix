09463632abf3edc23172e18b912f72078497e192
kaan-w <105508453+kaan-w@users.noreply.github.com>
2025-10-13 21:27:33 +0300

    update rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,49 +1,35 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     naersk = {
       url = "github:nix-community/naersk";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    git-hooks = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, naersk, git-hooks, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk {};
+
+  outputs = { flake-parts, ... }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+
+      perSystem = { pkgs, ... }: let
+        naersk-lib = pkgs.callPackage inputs.naersk {};
       in {
-        packages.default = naersk-lib.buildPackage {
+        packages = naersk-lib.buildPackage {
           src = ./.;
         };
 
-        checks.pre-commit = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            clippy.enable = true;
-            clippy.settings.denyWarnings = true;
-            rustfmt.enable = true;
-          };
-        };
-
-       formatter = pkgs.writeShellApplication {
-          name = "pre-commit-run";
-          text = ''
-            ${self.checks.${system}.pre-commit.config.package}/bin/pre-commit run --all-files
-          '';
-        };
-
-        devShells.default = let
-          inherit (self.checks.${system}.pre-commit) shellHook enabledPackages;
-        in pkgs.mkShell {
-          inherit shellHook;
-          buildInputs = with pkgs; [ rustc cargo ] ++ enabledPackages;
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            rustc 
+            cargo
+            rustfmt
+            clippy
+          ];
+          
           env.RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
         };
-      }
-    );
+      };
+    };
 }
+
