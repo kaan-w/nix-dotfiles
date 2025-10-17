e1846df0f310152c1dfae0c679b1bfb8ac588cc3
kaan-w <105508453+kaan-w@users.noreply.github.com>
2025-10-17 15:42:26 +0300

    update zig template
    - switch to zig nightly release
    - delete build.zig

--- templates/zig/flake.nix
+++ templates/zig/flake.nix
@@ -2,17 +2,18 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
   outputs = { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-      perSystem = { pkgs, ... }: {
+      perSystem = { pkgs, inputs', ... }: {
         devShells.default = pkgs.mkShellNoCC {
-          buildInputs = with pkgs; [
-            zig
-            zls
+          buildInputs = [
+            inputs'.zig.packages.master
+            pkgs.zls
           ];
         };
       };
